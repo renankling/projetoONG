aba6ae988e136ab694fae24f3c612867d87b9d79
renankling <renankling00@gmail.com>
2025-11-12 00:38:34 -0300

    Apply stash: resolvendo conflitos

--- uca/settings.py
+++ uca/settings.py
@@ -94,7 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -112,6 +112,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+
 
 
 import os
